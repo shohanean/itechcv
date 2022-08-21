9ac7f92b17dca8f6bb2a96f5807e34e19a7c363c
unknown <citrwd2107@gmail.com>
2022-08-21 15:02:09 +0600

    pagination added

--- resources/views/backend/request_from_employer.blade.php
+++ resources/views/backend/request_from_employer.blade.php
@@ -56,6 +56,9 @@
                 @endforeach
               </tbody>
             </table>
+            <div class="mt-4">
+                {{$cvrequests->links()}}
+            </div>
           </div>
         </div>
       </div>
